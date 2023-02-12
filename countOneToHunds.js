4891bf007d83e697b224d03d6b8170367acecf35
Y8a0 <124534969+Y8a0@users.noreply.github.com>
2023-02-12 15:02:24 +0100

    Update countOneToHunds.js

--- countOneToHunds.js
+++ countOneToHunds.js
@@ -1,9 +1,9 @@
-function decompte(i){
+function countOneToHunds(i){
     var i=1;
     for(i=1;i<100;i++){
        console.log(i);   
     }
     return i;
 }
-console.log(decompte(100));
+console.log(countOneToHunds(100));
 module.exports = countOneToHunds;
